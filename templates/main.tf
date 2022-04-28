7a38e6bfc6b7c3943bb00cfc8c9099ae5e51c4fd
Bob Clarke <bobclarke@users.noreply.github.com>
2022-04-28 09:54:08 +0100

    Update main.tf

--- templates/main.tf
+++ templates/main.tf
@@ -6,17 +6,16 @@ variable "envwhitelistmap" {
 }
 
 locals {
-   //a = substr(var.envwhitelistmap.ips, 1, length(var.envwhitelistmap.ips) - 2) 
 
    
-   //envwhitelist = regexall("\\d+.\\d+.\\d+.\\d+/\\d+", var.envwhitelistmap.ips)
+   envwhitelist = regexall("\\d+.\\d+.\\d+.\\d+/\\d+", var.envwhitelistmap.ips)
    rendered_config = templatefile("${path.module}/config/istio.aks.yaml", { ip_addrs = var.envwhitelistarry } )
 }
 
-/*
+
 output "envwhitelist_rendered" {
-    //value = local.rendered_config
+    value = local.rendered_config
     //value = local.envwhitelist
 }
-*/
+
 
